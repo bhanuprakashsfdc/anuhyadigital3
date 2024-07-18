16cd58f0e66dd732c44c1cb64f72be3a9b06a417
bhanuprakashsfdc <bhanusunny7@gmail.com>
2024-07-18 08:26:12 +0530

    Pushing website to make live

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
       <AboutSection />
       <Services />
       <ProjectSection />
-      <TeamSection />
+     {/* <TeamSection /> */} 
       <WorkingProcessSection />
       <ClientSection />
       <TestimonialsSection />
